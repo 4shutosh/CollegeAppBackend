69df85d2cca5df85d0acaf0ba6ac96a98cd81983
4shutosh <4shutoshsingh@gmail.com>
2021-10-12 20:45:27 +0530

    added constants for query

--- src/main/kotlin/com/collegeapp/routes/UserRoute.kt
+++ src/main/kotlin/com/collegeapp/routes/UserRoute.kt
@@ -1,6 +1,12 @@
 package com.collegeapp.routes
 
 import com.collegeapp.models.User
+import com.collegeapp.utils.Constants.EMAIL
+import com.collegeapp.utils.Constants.IMAGE_URL
+import com.collegeapp.utils.Constants.MONGO_DB_NAME
+import com.collegeapp.utils.Constants.NAME
+import com.collegeapp.utils.Constants.ROUTE_USER
+import com.collegeapp.utils.Constants.USER_ID
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
@@ -11,15 +17,15 @@ import org.litote.kmongo.reactivestreams.KMongo
 
 object UserRoute {
     private val client = KMongo.createClient().coroutine
-    private val database = client.getDatabase("college_app_db")
+    private val database = client.getDatabase(MONGO_DB_NAME)
     private val userCollection = database.getCollection<User>()
 
     fun Route.loginOrCreateUser() {
-        get("/users") {
-            val userId = call.parameters["userId"]?.toLong()
-            val userEmail = call.parameters["email"].toString()
-            val userName = call.parameters["name"].toString()
-            val userImageUrl = call.parameters["imageUrl"].toString()
+        get("/$ROUTE_USER") {
+            val userId = call.parameters[USER_ID]?.toLong()
+            val userEmail = call.parameters[EMAIL].toString()
+            val userName = call.parameters[NAME].toString()
+            val userImageUrl = call.parameters[IMAGE_URL].toString()
 
             if (userId == 69L) {
                 call.respondText(
@@ -28,7 +34,6 @@ object UserRoute {
                 )
                 return@get
             }
-
 
             // todo check if imageUrl is necessary
             if (userId == null || userEmail.isEmpty() || userName.isEmpty() || userImageUrl.isEmpty()) {
---
+++ src/main/kotlin/com/collegeapp/utils/Constants.kt
@@ -0,0 +1,14 @@
+package com.collegeapp.utils
+
+object Constants {
+
+    const val MONGO_DB_NAME = "college_app_db"
+
+
+    const val USER_ID = "userId"
+    const val NAME = "name"
+    const val EMAIL = "email"
+    const val IMAGE_URL = "image_url"
+
+    const val ROUTE_USER = "users"
+}
